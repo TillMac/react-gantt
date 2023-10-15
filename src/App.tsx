66e870d496ab4a1898d6258b95e5c992d6dbb601
TillMac <danny890308@gmail.com>
2023-10-15 15:47:17 +0800

    feat: 完成 AddTaskModal 切版

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import AddTaskModal from './components/AddTaskModal'
 import Sidebar from './components/Sidebar'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className='w-screen'>
       <Sidebar />
+      <AddTaskModal />
     </div>
   )
 }
