6c3587b20f2bf60efeaab2143b6d2c4642736079
TillMac <danny890308@gmail.com>
2023-10-25 22:09:23 +0800

    feat: 完成 LoginPage 切版

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import loginImage from '@/assets/logo.png';
+import loginPageBg from '@/assets/loginBg.jpg';
+import { Button } from '@/components/ui/button';
+
+const LoginPage = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col gap-20 bg-cover bg-gray-400 bg-blend-multiply justify-center select-none' style={{backgroundImage: `url(${loginPageBg})`}}>
+      <section className='flex gap-6 justify-center'>
+        <img src={loginImage} className='bg-white rounded-xl w-20 inline' />
+        <h2 className='inline font-mono text-4xl mt-auto mb-0'>Ticket.<span className='text-theme'>Gantt</span></h2>
+      </section>
+      <h3 className='text-6xl mix-blend-difference'>Start Your Personal Project Management from NOW.</h3>
+      <Button className='w-1/8 mx-auto bg-theme rounded-xl text-white hover:bg-white hover:text-theme'>Sign in with Google</Button>
+    </div>
+  )
+}
+
+export default LoginPage
