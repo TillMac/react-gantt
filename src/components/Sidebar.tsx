c11263fade6a0cf72ebe80276387798a3de070b8
TillMac <danny890308@gmail.com>
2023-10-27 03:49:01 +0800

    feat: 新增登出功能

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,20 @@ import { faArrowRightFromBracket, faTableCellsLarge } from '@fortawesome/free-so
 import ProjectList from './ProjectList';
 import { Button } from './ui/button';
 import AddingProject from './AddingProject';
+import { googleLogout } from '@react-oauth/google';
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
 
 const Sidebar = () => {
+  const { setAuthenticated } = useContext(AuthContext);
+  const logoutGoogle = () => {
+    googleLogout();
+    setAuthenticated({
+      isAuthenticated: false,
+      accessToken: '',
+    })
+  };
+
   return (
     <div className="w-72 flex flex-col gap-4 bg-gray-200 h-screen sticky z-10 p-0 mt-0 ml-0 mr-auto border-r-2 border-gray">
       <section className="w-full pl-6 pt-6 flex flex-wrap items-center">
@@ -16,7 +28,7 @@ const Sidebar = () => {
       <ProjectList />
       <section className='w-full px-4 py-2 mb-0 mt-auto flex items-center border-t-2 border-gray'>
         <AddingProject />
-        <Button className='m-0 hover:bg-gray hover:border-gray rounded-full ml-auto mr-0'>
+        <Button className='m-0 hover:bg-gray hover:border-gray rounded-full ml-auto mr-0' onClick={() => logoutGoogle()}>
           <FontAwesomeIcon icon={faArrowRightFromBracket} className='text-text text-xl' />
         </Button>
       </section>
