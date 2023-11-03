398de4aabfb967fe3f208c7436f0818efb851ccc
TillMac <danny890308@gmail.com>
2023-11-03 16:22:35 +0800

    fix: 修改 hook 使其能觸發每一次的 fetch

--- src/hooks/useProjectListFetch.tsx
+++ src/hooks/useProjectListFetch.tsx
@@ -1,5 +1,5 @@
 import { HttpMethod, IProject, fetchUrl } from '@/models/common';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 type RequestSchema  = {
   uId: string,
@@ -20,52 +20,57 @@ const useProjectListFetch = (initialUId: UId = null, initialAccessToken: AccessT
   const [accessToken, setAccessToken] = useState<AccessToken>(initialAccessToken);
   const [method, setMethod] = useState<HttpMethod>(initialMethod);
   const [body, setBody] = useState<IProject>(initialBody);
+  const [requestCount, setRequestCount] = useState<number>(0);
+
+  const fetchProjectList = useCallback(async () => {
+    setIsLoading(true);
+    const url = `${fetchUrl}${uId}/projects.json?auth=${accessToken}`
+    console.log('fetch 起來！');
+
+    try {
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body ? JSON.stringify(body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
+      // 若是 DELETE method 可能沒有回傳內容
+      const responseData = method === 'DELETE' ? {} : await response.json();
+      const projectsArray: IProject[] = Object.values(responseData);
+      setData(projectsArray);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [uId, accessToken, method, body]);  
 
   // 使用 useEffect 來觸發 fetch
   useEffect(() => {
+    console.log('難道是沒有 uId?', uId);
     if (!uId) return;
+    fetchProjectList();
+  }, [fetchProjectList, requestCount, uId]); // 當 uId、method 或 body 改變時，重新執行 useEffect
 
-    const fetchData = async () => {
-      setIsLoading(true);
-      const url = `${fetchUrl}${uId}/projects.json?auth=${accessToken}`
-
-      try {
-        const response = await fetch(url, {
-          method: method,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: body ? JSON.stringify(body) : null,
-        });
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
-        }
-
-        // 若是 DELETE method 可能沒有回傳內容
-        const responseData = method === 'DELETE' ? {} : await response.json();
-        const projectsArray: IProject[] = Object.values(responseData);
-        setData(projectsArray);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [uId, accessToken, method, body]); // 當 uId、method 或 body 改變時，重新執行 useEffect
+  const setRequest = useCallback(({ uId, method = 'GET', accessToken = null, body = null }: RequestSchema) => {
+    setUId(uId);
+    setMethod(method);
+    setAccessToken(accessToken);
+    setBody(body);
+    setRequestCount(currentCount => currentCount + 1);
+  }, []);
 
   return {
     data,
     error,
     isLoading,
-    setRequest: ({ uId, method = 'GET', accessToken = null, body = null }: RequestSchema) => {
-      setUId(uId);
-      setMethod(method);
-      setAccessToken(accessToken);
-      setBody(body);
-    },
+    setRequest,
   };
 };
 
