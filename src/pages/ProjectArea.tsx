0a4a26ea07cf7ab71967ce167fe7ebba67cd9d9c
TillMac <danny890308@gmail.com>
2023-11-04 18:00:26 +0800

    feat: 完成 ProjectArea 切版

--- src/pages/ProjectArea.tsx
+++ src/pages/ProjectArea.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { faPenToSquare, faStar, faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const projectNav: Record<string, ReactNode> = {
+  star: <FontAwesomeIcon icon={faStar} className='text-2xl hover:text-yellow-300 hover:cursor-pointer' />,
+  edit: <FontAwesomeIcon icon={faPenToSquare} className='text-2xl hover:cursor-pointer hover:text-theme' />,
+  delete: <FontAwesomeIcon icon={faTrashCan} className='text-2xl hover:cursor-pointer hover:text-red-500' />
+};
 
 const ProjectArea = () => {
   return (
     <div>ProjectArea</div>
+    <>
+      <section className="w-full flex justify-between items-center">
+        <h2 className='text-3xl'>Project_Name</h2>
+        <div className="flex gap-3">
+          {
+            Object.keys(projectNav).map((item: string) => {
+              return (
+                <div key={item}>
+                  {projectNav[item]}
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
