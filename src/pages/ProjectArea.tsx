ae623c47e0bf8cc32bee0887d37c0801512193bd
TillMac <danny890308@gmail.com>
2023-11-07 14:50:13 +0800

    fix: ProjectSetting 取代原有 code

--- src/pages/ProjectArea.tsx
+++ src/pages/ProjectArea.tsx
@@ -1,13 +1,5 @@
 import ViewModeSelector from '@/components/ViewModeSelector';
-import { faPenToSquare, faStar, faTrashCan } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { ReactNode, useState } from 'react'
-
-const projectNav: Record<string, ReactNode> = {
-  star: <FontAwesomeIcon icon={faStar} className='text-2xl hover:text-yellow-300 hover:cursor-pointer' />,
-  edit: <FontAwesomeIcon icon={faPenToSquare} className='text-2xl hover:cursor-pointer hover:text-theme' />,
-  delete: <FontAwesomeIcon icon={faTrashCan} className='text-2xl hover:cursor-pointer hover:text-red-500' />
-};
+import ProjectSetting from '@/components/ProjectSetting';
 
 const ProjectArea = () => {
   const [viewMode, setViewMode] = useState<number>(1);
@@ -15,18 +7,7 @@ const ProjectArea = () => {
   return (
     <>
       <section className="w-full flex justify-between items-center">
-        <h2 className='text-3xl'>Project_Name</h2>
-        <div className="flex gap-3">
-          {
-            Object.keys(projectNav).map((item: string) => {
-              return (
-                <div key={item}>
-                  {projectNav[item]}
-                </div>
-              )
-            })
-          }
-        </div>
+        <ProjectSetting project={activeProject} setReloadProjectListData={setReloadProjectListData} />
       </section>
       <ViewModeSelector viewMode={viewMode} setViewMode={setViewMode} />
     </>
