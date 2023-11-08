10db07a089fdae6bf82e1f62e306006310bf7a28
TillMac <danny890308@gmail.com>
2023-11-08 16:52:46 +0800

    feat: 完成 GET API，並使其在對應情況下自動更新

--- src/pages/ProjectArea.tsx
+++ src/pages/ProjectArea.tsx
@@ -2,11 +2,41 @@ import ViewModeSelector from '@/components/ViewModeSelector';
 import ProjectSetting from '@/components/ProjectSetting';
 import { useActiveProject } from '@/layouts/AppLayout';
 import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddingTask from '@/components/AddingTask';
+import useProjectFetch from '@/hooks/useProjectFetch';
+import { useAuth } from '@/context/AuthContext';
+import GanttChart from '@/components/GanttChart';
 
 const ProjectArea = () => {
   const [viewMode, setViewMode] = useState<number>(1);
   const { activeProject, setReloadProjectListData } = useActiveProject();
+  const [reloadProjectData, setReloadProjectData] = useState<boolean>(false);
+  const { data, setRequest } = useProjectFetch();
+  const { currentUser } = useAuth();
+
+  const getProjectData = () => {
+    if (activeProject) {
+      setRequest({
+        uId: currentUser.uid,
+        projectId: activeProject!.id,
+        method: 'GET',
+        accessToken: currentUser.accessToken,
+      });
+    }
+    setReloadProjectData(false);
+  }
+
+  useEffect(() => {
+    getProjectData();
+  }, []);
+
+  useEffect(() => {
+    if (reloadProjectData) {
+      getProjectData();
+    }
+  }, [reloadProjectData])
+  
   
   return (
     <>
