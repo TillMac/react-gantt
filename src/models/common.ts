e29d2d7371cf339b06b7413c49d7a74943e758f0
TillMac <danny890308@gmail.com>
2023-11-08 16:53:25 +0800

    chore: 移除 IMilestone 型別，統一使用 ITask

--- src/models/common.ts
+++ src/models/common.ts
@@ -20,27 +20,12 @@ const ITaskSchema = z.object({
   description: z.string(),
   projectId: z.string().uuid(),
   progress: z.number().min(0).max(100),
-  type: z.literal('task'),
+  type: z.union([z.literal('task'), z.literal('milestone')]),
   createTime: z.date().optional(),
   updateTime: z.date().optional(),
 }).nullable();
 
 type ITask = z.infer<typeof ITaskSchema>;
-
-const IMilestoneSchema = z.object({
-  name: z.string(),
-  id: z.string().uuid(),
-  startDate: z.date(),
-  dueDate: z.date(),
-  description: z.string(),
-  projectId: z.string().uuid(),
-  progress: z.number().min(0).max(100),
-  type: z.literal('milestone'),
-  createTime: z.date().optional(),
-  updateTime: z.date().optional(),
-}).nullable();
-
-type IMilestone = z.infer<typeof IMilestoneSchema>;
 
 type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
@@ -52,5 +37,5 @@ enum viewModes {
 
 const fetchUrl = 'https://ticket-gantt-default-rtdb.firebaseio.com/';
 
-export type { IProject, ITask, IMilestone, HttpMethod };
+export type { IProject, ITask, HttpMethod };
 export { fetchUrl, viewModes };
