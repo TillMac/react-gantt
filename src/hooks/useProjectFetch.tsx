14a24895ffe1eef7eb2c440b8f2cb3ba10b2574f
TillMac <danny890308@gmail.com>
2023-11-08 16:50:47 +0800

    fix: 修正 custom fetch hook 以符合 API 呼叫場景

--- src/hooks/useProjectFetch.tsx
+++ src/hooks/useProjectFetch.tsx
@@ -1,36 +1,41 @@
-import { HttpMethod, IMilestone, ITask, fetchUrl } from '@/models/common';
-import { useState, useEffect } from 'react';
-
-type ProjectSchema = ITask | IMilestone;
+import { HttpMethod, ITask, fetchUrl } from '@/models/common';
+import { useState, useEffect, useCallback } from 'react';
 
 type ProjectId = string | null;
+type TaskId = string | null;
 
 type RequestSchema = {
   uId: string,
   projectId: string,
   method: HttpMethod,
-  body: ProjectSchema,
+  taskId?: string,
+  accessToken?: string | null,
+  body?: ITask,
 };
 
 type UId = null | string;
+type AccessToken = null | string;
 
-const useProjectFetch = (initialUId: UId = null, initialProjectId: ProjectId = null, initialMethod: HttpMethod = 'GET', initialBody: ProjectSchema = null) => {
+const useProjectFetch = (initialUId: UId = null, initialProjectId: ProjectId = null, initialMethod: HttpMethod = 'GET', initialAccessToken: AccessToken = '', initialBody: ITask = null) => {
   // 狀態管理
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<ITask[] | null>(null);
   const [error, setError] = useState<unknown>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [uId, setUId] = useState<UId>(initialUId);
+  const [accessToken, setAccessToken] = useState<AccessToken>(initialAccessToken);
   const [projectId, setProjectId] = useState<ProjectId>(initialProjectId);
+  const [taskId, setTaskId] = useState<TaskId>(null)
   const [method, setMethod] = useState<HttpMethod>(initialMethod);
-  const [body, setBody] = useState<ProjectSchema>(initialBody);
+  const [body, setBody] = useState<ITask>(initialBody);
+  const [requestcount, setRequestCount] = useState<number>(0);
 
-  // 使用 useEffect 來觸發 fetch
-  useEffect(() => {
-    if (!uId) return;
-
-    const fetchData = async () => {
+  const fetchProjectData = useCallback(
+    async () => {
       setIsLoading(true);
-      const url = `${fetchUrl}/${uId}/${projectId}.json`
+      let url = `${fetchUrl}${uId}/${projectId}.json?auth=${accessToken}`
+      if (method !== 'GET') {
+        url = `${fetchUrl}${uId}/${projectId}/${taskId}.json?auth=${accessToken}`
+      }
 
       try {
         const response = await fetch(url, {
@@ -46,28 +51,40 @@ const useProjectFetch = (initialUId: UId = null, initialProjectId: ProjectId = n
         }
 
         // 若是 DELETE method 可能沒有回傳內容
-        const responseData = method === 'DELETE' ? {} : await response.json();
-        setData(responseData);
+        const responseData = method === 'DELETE' ? {message: 'Delete susscessfully.'} : await response.json();
+        if (!responseData) {
+          setData(null);
+        }
+        const tasksArray: ITask[] = Object.values(responseData);
+        setData(tasksArray);
       } catch (error) {
         setError(error);
       } finally {
         setIsLoading(false);
       }
-    };
+    }, [accessToken, body, method, projectId, uId, taskId]);
+  
+  // 使用 useEffect 來觸發 fetch
+  useEffect(() => {
+    if (!uId) return;
+    fetchProjectData();
+  }, [projectId, taskId, accessToken, requestcount, method, body, uId, fetchProjectData]); // 當 uId、method 或 body 改變時，重新執行 useEffect
 
-    fetchData();
-  }, [uId, projectId, method, body]); // 當 uId、method 或 body 改變時，重新執行 useEffect
+  const setRequest = useCallback(({ uId, method = 'GET', projectId = '', taskId = '', accessToken = null, body = null}: RequestSchema) => {
+    setUId(uId);
+    setMethod(method);
+    setProjectId(projectId);
+    setTaskId(taskId);
+    setAccessToken(accessToken);
+    setBody(body);
+    setRequestCount(currentCount => currentCount + 1);
+  }, []);
 
   return {
     data,
     error,
     isLoading,
-    setRequest: ({ uId, projectId, method = 'GET', body = null }: RequestSchema) => {
-      setUId(uId);
-      setProjectId(projectId),
-      setMethod(method);
-      setBody(body);
-    },
+    setRequest,
   };
 };
 
