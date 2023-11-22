2809f278efc7f08d41d0eac9da9d5ae7101144b7
TillMac <danny890308@gmail.com>
2023-11-22 15:03:57 +0800

    fix: 修改 ITask 部分型別

--- src/models/common.ts
+++ src/models/common.ts
@@ -15,15 +15,15 @@ type IProject = z.infer<typeof IProjectSchema>;
 const ITaskSchema = z.object({
   name: z.string(),
   id: z.string().uuid(),
-  startDate: z.date(),
-  dueDate: z.date(),
+  start: z.date(),
+  end: z.date(),
   description: z.string(),
-  projectId: z.string().uuid(),
+  project: z.string().uuid(),
   progress: z.number().min(0).max(100),
   type: z.union([z.literal('task'), z.literal('milestone')]),
-  createTime: z.date().optional(),
-  updateTime: z.date().optional(),
-}).nullable();
+  createTime: z.union([z.date(), z.string()]).optional(),
+  updateTime: z.union([z.date(), z.string()]).optional(),
+});
 
 type ITask = z.infer<typeof ITaskSchema>;
 
