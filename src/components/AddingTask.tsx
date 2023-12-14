8745ddc098ef8ed280b89c7e503d7501d7a9e028
TillMac <danny890308@gmail.com>
2023-12-14 15:06:37 +0800

    chore: 整理程式碼，取出可復用型別

--- src/components/AddingTask.tsx
+++ src/components/AddingTask.tsx
@@ -19,39 +19,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 import React, { Dispatch, SetStateAction } from 'react'
 import { DialogClose } from '@radix-ui/react-dialog'
-
-const taskLabel: Record<string, string> = {
-  taskName: 'Task Name',
-  status: 'Status',
-  start: 'Start Date',
-  end: 'Due Date',
-  type: 'Type',
-  progress: 'Progress (%)',
-};
-
-type taskFormInputNameType = "taskName";
-type taskFormDateNameType = "start" | "end";
-
-const taskFormSchema = z.object({
-  taskName: z.string().min(1, {
-    message: 'Task name must be at least 1 character, and lower than 20 characters.',
-  }).max(20),
-  status: z.union([z.literal('TODO'), z.literal('IN PROGRESS'), z.literal('DONE'), z.literal('WAIVED')]),
-  start: z.date({
-    required_error: "A date of start date is required.",
-  }),
-  end: z.date({
-    required_error: "A date of due date is required.",
-  }),
-  type: z.union([z.literal('task', {
-    description: '請輸入 task 或 milestone',
-  }), z.literal('milestone')], {
-    description: '請輸入 task 或 milestone',
-  }).default('task'),
-  progress: z.number().min(0, {
-    message: 'The progress(%) must between 0 and 100.'
-  }).max(100)
-});
+import { taskFormDateNameType, taskFormInputNameType, taskFormSchema, taskLabel } from '@/models/taskForm'
 
 type Props = {
   project: IProject,
